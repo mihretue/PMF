a0b5837e70e03b26cd57955eeb73c750a8412a7b
SosiSis <sosinasisay30@gmail.com>
2025-04-01 20:23:05 +0300

    navigation

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,12 @@
-
+import Link from 'next/link';
 export default function Home() {
   return (
     <div>
       Hello World
+      <Link href='/login'>login</Link>
+      <Link href='/registration'>register</Link>
+      <Link href='/reset-password/token'>password</Link>
+      <Link href='/service'>service</Link>
     </div>
   );
 }
