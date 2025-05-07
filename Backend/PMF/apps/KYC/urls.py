a623c13f3181914a1893e27ca9bece458c26349c
mihretue <mihretuendeshaw84@gmail.com>
2025-05-07 15:30:38 +0300

    Add UserKYCStatusView to retrieve user's KYC verification status

--- Backend/PMF/apps/KYC/urls.py
+++ Backend/PMF/apps/KYC/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import KYCCreateView, KYCAdminUpdateView
+from .views import KYCCreateView, KYCAdminUpdateView, UserKYCStatusView
 
 
 urlpatterns = [
    path('submit-kyc/', KYCCreateView.as_view(), name='submit-kyc'),
+   path('status/', UserKYCStatusView.as_view(), name='kyc-status'),
    path('admin/update-kyc/<int:kyc_id>/', KYCAdminUpdateView.as_view(), name='update-kyc'),
 ]
--- Backend/PMF/apps/KYC/views.py
+++ Backend/PMF/apps/KYC/views.py
@@ -16,6 +16,19 @@ class KYCCreateView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class UserKYCStatusView(APIView):
+    permission_classes = [IsSenderOrReceiver]
+
+    def get(self, request):
+        try:
+            kyc = KYC.objects.get(user=request.user)
+            return Response({
+                "verification_status": kyc.verification_status
+            }, status=status.HTTP_200_OK)
+        except KYC.DoesNotExist:
+            return Response({
+                "verification_status": "Not Submitted"
+            }, status=status.HTTP_404_NOT_FOUND)
 
 class KYCAdminUpdateView(APIView):
     permission_classes = [IsAdmin]
