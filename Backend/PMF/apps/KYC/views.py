45a348c5a3153517bdfee70436de4477fd07f6f3
mihretue <mihretuendeshaw84@gmail.com>
2025-05-10 21:41:02 +0300

    Add KYC update and detail views to manage user KYC records

--- Backend/PMF/apps/KYC/views.py
+++ Backend/PMF/apps/KYC/views.py
@@ -40,3 +40,63 @@ class KYCAdminUpdateView(APIView):
 
         # Notify user via email (Optional)
         return Response({"message": "KYC status updated successfully"}, status=status.HTTP_200_OK)
+
+
+#Put request
+
+class KYCUpdateView(APIView):
+    permission_classes = [IsSenderOrReceiver]
+
+    def put(self, request):
+        try:
+            kyc = KYC.objects.get(user=request.user)
+        except KYC.DoesNotExist:
+            return Response({"error": "KYC record not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        if kyc.verification_status != "pending":
+            return Response({"error": "KYC cannot be edited as it is not in 'Pending' status."},
+                            status=status.HTTP_403_FORBIDDEN)
+
+        serializer = KYCSerializer(kyc, data=request.data, partial=False, context={'request': request})
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# patch request
+class KYCPartialUpdateView(APIView):
+    permission_classes = [IsSenderOrReceiver]
+
+    def patch(self, request):
+        try:
+            kyc = KYC.objects.get(user=request.user)
+        except KYC.DoesNotExist:
+            return Response({"error": "KYC record not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        if kyc.verification_status != "pending":
+            return Response({"error": "KYC cannot be edited as it is not in 'Pending' status."},
+                            status=status.HTTP_403_FORBIDDEN)
+
+        serializer = KYCSerializer(kyc, data=request.data, partial=True, context={'request': request})
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Get Request 
+
+class UserKYCDetailView(APIView):
+    permission_classes = [IsSenderOrReceiver]
+
+    def get(self, request):
+        try:
+            kyc = KYC.objects.get(user=request.user)
+            serializer = KYCSerializer(kyc)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except KYC.DoesNotExist:
+            return Response({"error": "KYC record not found."}, status=status.HTTP_404_NOT_FOUND)
