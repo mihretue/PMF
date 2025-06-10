657d982d3b716acd2355d0914b3d29f28b88f864
mihretue <mihretuendeshaw84@gmail.com>
2025-06-10 22:39:11 +0300

    Refactor code structure for improved readability and maintainability

--- Backend/PMF/PMF/asgi.py
+++ Backend/PMF/PMF/asgi.py
@@ -1,25 +1,31 @@
+"""
+ASGI config for PMF project.
 
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
 
 import os
 import django
 
-# 1. Set the Django settings module environment variable FIRST
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PMF.settings')
-
-# 2. Initialize Django's app registry IMMEDIATELY AFTER setting the module
-django.setup()
-
-# 3. NOW, you can safely import anything that depends on Django apps and models
-#    This includes get_asgi_application() which needs the app registry to be ready.
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from PMF.middleware import TokenAuthMiddlewareStack
 
+# Import your custom middleware and websocket routing
+from PMF.middleware import TokenAuthMiddlewareStack
 import apps.chatting.routing
 
+# Set the Django settings module environment variable
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PMF.settings')
 
+# Initialize Django apps registry
+django.setup()
+
+# Define ASGI application
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(), # get_asgi_application() is now defined because it's imported above
+    "http": get_asgi_application(),
     "websocket": TokenAuthMiddlewareStack(
         URLRouter(
             apps.chatting.routing.websocket_urlpatterns
